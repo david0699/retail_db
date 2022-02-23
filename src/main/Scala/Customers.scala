e8c5c2739edf1f2f03ce0c27447a96a246ffc22d
David Gómez Soria <davidgoso@hotmail.es>
2022-02-23 14:34:05 +0100

    Create Customers object

--- src/main/Scala/Customers.scala
+++ src/main/Scala/Customers.scala
@@ -1,6 +1,14 @@
+import Main.sparksession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 object Customers {
 
-
+def readCustomersCsv()(implicit sparkSession: SparkSession):DataFrame={
+  sparksession
+    .read
+    .option("header","true")
+    .csv("src/main/resources/retail_db/customers/part-m-00000 - Copy")
+}
 
 
 }
--- src/main/Scala/Main.scala
+++ src/main/Scala/Main.scala
@@ -1,14 +1,11 @@
 import org.apache.spark.sql.SparkSession
 
 object Main extends App {
-  val sparksession = Spark.getSparkSession
+  implicit val sparksession = Spark.getSparkSession
 
 
   sparksession.sparkContext.setLogLevel("ERROR")
-  val customers = sparksession
-    .read
-    .option("header","true")
-    .csv("src/main/resources/retail_db/customers/part-m-00000 - Copy")
+  val customers = Customers.readCustomersCsv()
 
   customers.show()
 }
