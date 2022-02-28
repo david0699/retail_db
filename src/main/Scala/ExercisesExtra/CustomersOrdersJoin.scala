fb30dcdd3bbd90eda854599c090bc460d06e7e21
David Gómez Soria <davidgoso@hotmail.es>
2022-02-28 11:57:10 +0100

    Extra Exercises-Modify CustomersOrdersJoin(improve code)

--- src/main/Scala/ExercisesExtra/CustomersOrdersJoin.scala
+++ src/main/Scala/ExercisesExtra/CustomersOrdersJoin.scala
@@ -15,19 +15,18 @@ object CustomersOrdersJoin {
           .as("customer_name"))
 
     val orders = Orders.getOrders()
-    customers.printSchema()
-    customers.show()
-    orders.printSchema()
-    orders.show()
 
     val customerOrders = customers.join(orders,customers("customer_id") === orders("_c2"),"left")
       .select(col("customer_id"),col("customer_name"))
       .groupBy("customer_id")
       .count()
-      .orderBy(col("customer_id"))
-
 
     customers.join(customerOrders,customers("customer_id") === customerOrders("customer_id"),"inner")
-      .select(customers("customer_id"),col("customer_name"),col("count").as("orders_count")).dropDuplicates("customer_id").orderBy(col("customer_id")).show()
+      .select(customers("customer_id")
+        ,col("customer_name")
+        ,col("count").as("orders_count"))
+      .dropDuplicates("customer_id")
+      .orderBy(col("customer_id"))
+      .show()
   }
 }
